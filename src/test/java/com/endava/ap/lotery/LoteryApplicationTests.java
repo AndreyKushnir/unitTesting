97bb92bdf1af8878bc8552491e84ba1f6c1b9084
dvicol <vicoldorian@gmail.com>
2019-03-04 11:43:37 +0200

    init commit

---
+++ src/test/java/com/endava/ap/lotery/LoteryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.endava.ap.lotery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoteryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
